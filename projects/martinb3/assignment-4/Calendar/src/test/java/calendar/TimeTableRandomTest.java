b4bee27cb2aa532da6f29a66509ae1dc14a76220
tonyenike <martinb3@flip3.engr.oregonstate.edu>
2018-02-26 23:29:01 -0800

    Almost everything

--- projects/martinb3/assignment-4/Calendar/src/test/java/calendar/TimeTableRandomTest.java
+++ projects/martinb3/assignment-4/Calendar/src/test/java/calendar/TimeTableRandomTest.java
@@ -3,6 +3,8 @@ package calendar;
 import java.util.Calendar;
 import java.util.Random;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -50,11 +52,14 @@ public class TimeTableRandomTest {
 	//			System.out.println(" Seed:"+randomseed );
 				Random random = new Random(randomseed);
 				
+
+
+
 				 int startHour=ValuesGenerator.getRandomIntBetween(random, -5, 30);
 				 int startMinute=ValuesGenerator.getRandomIntBetween(random, -5, 65);
 				 int startDay=ValuesGenerator.getRandomIntBetween(random, -5, 35);
 				 int startMonth=ValuesGenerator.getRandomIntBetween(random, -3, 14);
-				 int startYear=ValuesGenerator.RandInt(random);
+				 int startYear=ValuesGenerator.getRandomIntBetween(random, 2000, 2030);
 				 String title="Birthday Party";
 				 String description="This is my birthday party.";
 				 //Construct a new Appointment object with the initial data	 
@@ -67,19 +72,56 @@ public class TimeTableRandomTest {
 				         description);
 			 if(!appt.getValid())continue;
 			for (int i = 0; i < NUM_TESTS; i++) {
-					String methodName = ApptRandomTest.RandomSelectMethod(random);
+					String methodName = TimeTableRandomTest.RandomSelectMethod(random);
 					   if (methodName.equals("deleteAppt")){
 		
 
 
+
+
+
+
+
+
+
+
 						}
 					   else if (methodName.equals("getApptRange")){
+						LinkedList<Appt> huh = new LinkedList<Appt>();
+						for(int mm = 0; mm < 10; mm++){
+				 			startHour=ValuesGenerator.getRandomIntBetween(random, -1, 25);
+				 			startMinute=ValuesGenerator.getRandomIntBetween(random, -1, 61);
+				 			startDay=ValuesGenerator.getRandomIntBetween(random, -1, 32);
+				 			startMonth=ValuesGenerator.getRandomIntBetween(random, -1, 12);
+				 			startYear=ValuesGenerator.getRandomIntBetween(random, 2000, 2030);
 
+							appt.setStartHour(startHour);
+							appt.setStartMinute(startMinute);
+							appt.setStartDay(startDay);
+							appt.setStartMonth(startMonth);
+							appt.setStartYear(startYear);
+
+
+							huh.add(appt);
+						
+							}
+							TimeTable table = new TimeTable();
+
+				 			int someDay=ValuesGenerator.getRandomIntBetween(random, 1, 26);
+				 			int someMonth=ValuesGenerator.getRandomIntBetween(random, 2, 11);
+				 			int someYear=ValuesGenerator.getRandomIntBetween(random, 2000, 2015);
+							GregorianCalendar aday = new GregorianCalendar(someYear, someMonth, someDay);
+				 			someDay=ValuesGenerator.getRandomIntBetween(random, 1, 26);
+				 			someMonth=ValuesGenerator.getRandomIntBetween(random, 2, 11);
+				 			someYear=ValuesGenerator.getRandomIntBetween(random, 2016, 2030);
+							GregorianCalendar bday = new GregorianCalendar(someYear, someMonth, someDay);
+
+							table.getApptRange(huh, aday, bday);
 						}				
 				}
 				
-				 elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
-			        if((iteration%10000)==0 && iteration!=0 )
+				elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
+			        if((iteration%100)==0 && iteration!=0 )
 			              System.out.println("elapsed time: "+ elapsed + " of "+TestTimeout);
 			 
 			}
